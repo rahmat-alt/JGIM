93ef4fc3974635cb8835c8aaf88360d1b5ade39e
rahmat hidayat <rahmatthidayat0120@gmail.com>
2026-03-11 09:38:23 +0700

    penamaan

--- resources/views/script-partial.blade.php
+++ resources/views/script-partial.blade.php
@@ -1,4 +1,6 @@
+<!-- bootstrap js-->
 <script src="{{ asset('investment-maps/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js') }}"></script>
+<!-- assets js -->
 <script src="assets/js/main.js"></script>
-
+<!-- aos javascript -->
 <script src="{{ asset('jgim/packages/aos-master/dist/aos.js') }}"></script>
