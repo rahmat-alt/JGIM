1ba9097a8801642750c264019e75b77e3dda31e2
rahmat hidayat <rahmatthidayat0120@gmail.com>
2026-03-11 10:22:49 +0700

    penambahan asset

--- resources/views/link-partial.blade.php
+++ resources/views/link-partial.blade.php
@@ -6,3 +6,12 @@
 <link href="assets/css/main.css" rel="stylesheet">
 <!-- aos css -->
 <link rel="stylesheet" href="{{ asset('jgim/packages/aos-master/dist/aos.css') }}">
+<!-- swipper css -->
+<link rel="stylesheet" href="{{ asset('jgim/packages/package/swiper-bundle.min.css') }}">
+
+
+<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
